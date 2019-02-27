828acbf2a78f123bd58ebbc921c3b19e336a7a88
ren <renzhiwei@lewell.cn>
2019-02-27 16:03:47 +0800

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+import debounce from './methods/debounce'
+import {throttle} from './methods/throttle'
+import arrUnique from './methods/arrUnique'
+import sleep from './methods/sleep'
+
+export {
+  debounce,
+  throttle,
+  arrUnique,
+  sleep
+}
