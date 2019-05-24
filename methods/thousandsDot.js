1035956fa36f6450421a907c5014064753d515d5
klren <kuanglong0312@sina.com>
2019-05-24 17:25:02 +0800

    数字每三位打一个逗号

--- methods/thousandsDot.js
+++ methods/thousandsDot.js
@@ -3,7 +3,10 @@
  * @param {Number} num 
  */
 function thousandsDot(num) {
-	return num.toLocaleString();
+	return (+num || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','))
 }
+
+// 另一种方法
+// num.toLocaleString()
 
 export default thousandsDot
