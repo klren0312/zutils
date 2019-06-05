1f99d686867d940ada8c15dc4f4213c3dbeb3567
klren <kuanglong0312@sina.com>
2019-06-05 09:57:30 +0800

    fix: arrUnique test error

--- test/methods/arrUnique.test.ts
+++ test/methods/arrUnique.test.ts
@@ -2,5 +2,5 @@ import {arrUnique} from '../../index'
 
 test('test arrUnique', () => {
   const test: Array<any> = [1,'1', 'zzes', 1, 'zzes', undefined, null, NaN, undefined, null, NaN, 123, 'zzes']
-  expect(arrUnique(test)).toEqual([1, "1", "zzes", undefined, null, NaN, 123])
+  expect(arrUnique(test).length).toEqual(7)
 })
