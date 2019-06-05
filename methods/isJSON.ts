84be2e6260573318b0aecca2bf2316272fd4bbc0
klren <kuanglong0312@sina.com>
2019-06-05 15:54:02 +0800

    添加json判断

---
+++ methods/isJSON.ts
@@ -0,0 +1,17 @@
+function isJSON (str: string): boolean {
+  if (typeof str === 'string') {
+    try {
+      const obj: object = JSON.parse(str)
+      if (obj && typeof obj === 'object') {
+        return true
+      } else {
+        return false
+      }
+    } catch (e) {
+      return false
+    }
+  }
+  return false
+}
+
+export default isJSON
---
+++ test/methods/isJSON.test.ts
@@ -0,0 +1,8 @@
+import { isJSON } from '../../index'
+
+test('test isJSON', () => {
+  expect(isJSON('{"test":1}')).toBe(true)
+  expect(isJSON('"test"')).toBe(false)
+  expect(isJSON('null')).toBe(false)
+  expect(isJSON('test')).toBe(false)
+})
